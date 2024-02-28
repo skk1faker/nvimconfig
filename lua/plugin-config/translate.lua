601205b141acb6508d5812c9f1ae7229ac430c38
Wang Xinpeng <skk1faker@163.com>
2024-02-28 09:45:13 +0800

    增加了翻译软件配置文件

---
+++ lua/plugin-config/translate.lua
@@ -0,0 +1,7 @@
+vim.cmd([[
+let g:translator_target_lang='zh'
+let g:translator_source_lang='auto'
+let g:translator_default_engines = ['bing','google'] 
+let g:translator_proxy_url = 'socks5://127.0.0.1:7891'
+]])
+-- let g:translator_default_engines = ['bing', 'google'] 
